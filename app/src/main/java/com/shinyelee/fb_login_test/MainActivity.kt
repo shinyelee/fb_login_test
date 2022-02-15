bc2de5484251b72e1032bb8e716871a694e0e8f9
shinyelee <shinye0213@naver.com>
2022-02-15 22:59:12 +0900

    firebase - email login (2)

--- app/src/main/java/com/shinyelee/fb_login_test/MainActivity.kt
+++ app/src/main/java/com/shinyelee/fb_login_test/MainActivity.kt
@@ -5,13 +5,21 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
 
         val joinBtn = findViewById<Button>(R.id.joinBtn)
         joinBtn.setOnClickListener {
@@ -21,6 +29,17 @@ class MainActivity : AppCompatActivity() {
 
             Log.d("MAIN", email.text.toString())
             Log.d("MAIN", password.text.toString())
+
+            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Toast.makeText(this, "success", Toast.LENGTH_LONG).show()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(this, "fail", Toast.LENGTH_LONG).show()
+                    }
+                }
 
         }
     }
