559dc2caaa676f088fdaa652484d1b66cde1691d
shinyelee <shinye0213@naver.com>
2022-02-16 21:43:39 +0900

    diet memo app - dialog (2)

--- app/src/main/java/com/shinyelee/fb_login_test/MainActivity.kt
+++ app/src/main/java/com/shinyelee/fb_login_test/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.shinyelee.fb_login_test
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.DatePicker
 import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,7 +26,27 @@ class MainActivity : AppCompatActivity() {
                 .setView(mDialogView)
                 .setTitle("운동 메모 다이얼로그")
 
-            mBuilder.show()
+            val mAlertDialog = mBuilder.show()
+
+            mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)?.setOnClickListener {
+
+                val today = GregorianCalendar()
+                val year : Int = today.get(Calendar.YEAR)
+                val month : Int = today.get(Calendar.MONTH)
+                val date : Int = today.get(Calendar.DATE)
+
+                val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener {
+                    override fun onDateSet(
+                        view: DatePicker?,
+                        year: Int,
+                        month: Int,
+                        dayOfMonth: Int
+                    ) {
+                        Log.d("MAIN", "${year}, ${month+1}, ${dayOfMonth}")
+                    }
+                }, year, month, date)
+                dlg.show()
+            }
         }
     }
 }
