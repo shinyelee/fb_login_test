45150ca192e1ebbed5709e6cd7e9f0fd2add5a21
shinyelee <shinye0213@naver.com>
2022-02-16 21:21:04 +0900

    diet memo app - dialog (1)

--- app/src/main/java/com/shinyelee/fb_login_test/MainActivity.kt
+++ app/src/main/java/com/shinyelee/fb_login_test/MainActivity.kt
@@ -2,6 +2,9 @@ package com.shinyelee.fb_login_test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +12,16 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val writeButton = findViewById<ImageView>(R.id.writeBtn)
+        writeButton.setOnClickListener {
+
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog, null)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("운동 메모 다이얼로그")
+
+            mBuilder.show()
+        }
     }
 }
