2c05a7343d0339d9bc5b9020f91b91af01203956
shinyelee <shinye0213@naver.com>
2022-02-18 22:01:17 +0900

    Mangoplate app - bookmark (1)

---
+++ app/src/main/java/com/shinyelee/fb_login_test/JoinActivity.kt
@@ -0,0 +1,13 @@
+package com.shinyelee.fb_login_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JoinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
