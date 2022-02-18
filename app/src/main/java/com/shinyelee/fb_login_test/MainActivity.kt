ba71a1d850df6f89846c10c8b394a72fd6ce2e0a
shinyelee <shinye0213@naver.com>
2022-02-19 00:48:41 +0900

    Mangoplate app - bookmark (3)

--- app/src/main/java/com/shinyelee/fb_login_test/MainActivity.kt
+++ app/src/main/java/com/shinyelee/fb_login_test/MainActivity.kt
@@ -83,6 +83,9 @@ class MainActivity : AppCompatActivity() {
 
                 val intent = Intent(baseContext, ViewActivity::class.java)
                 intent.putExtra("url", items[position].url)
+                intent.putExtra("title", items[position].titleText)
+                intent.putExtra("imageUrl", items[position].imageUrl)
+
                 startActivity(intent)
             }
         }
