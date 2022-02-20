1d4620a30d7bc71477417fe446d013e4aeba855f
shinyelee <shinye0213@naver.com>
2022-02-21 01:01:55 +0900

    D-Day app - (2)startBtn & endBtn

--- app/src/main/java/com/shinyelee/fb_login_test/MainActivity.kt
+++ app/src/main/java/com/shinyelee/fb_login_test/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.shinyelee.fb_login_test
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.DatePicker
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.startBtn)
+        val endButton = findViewById<Button>(R.id.endBtn)
+
+        var startDate = ""
+        var endDate = ""
+
+        startButton.setOnClickListener {
+
+            val today = GregorianCalendar()
+            val year = today.get(Calendar.YEAR)
+            val month = today.get(Calendar.MONTH)
+            val day = today.get(Calendar.DATE)
+
+            val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener {
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+                    startDate = "${year} + ${month+1} + ${dayOfMonth}"
+//                  startDate = year.toString() + month+1.toString() + dayOfMonth.toString()과 동일
+                    Log.d("day : ", startDate)
+                }
+
+            }, year, month, day)
+            dlg.show()
+        }
+
+        endButton.setOnClickListener {
+
+            val today = GregorianCalendar()
+            val year = today.get(Calendar.YEAR)
+            val month = today.get(Calendar.MONTH)
+            val day = today.get(Calendar.DATE)
+
+            val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener {
+                override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+
+                    endDate = "${year} + ${month+1} + ${dayOfMonth}"
+//                  startDate = year.toString() + month+1.toString() + dayOfMonth.toString()과 동일
+                    Log.d("day : ", endDate)
+                }
+
+            }, year, month, day)
+            dlg.show()
+        }
     }
 }
