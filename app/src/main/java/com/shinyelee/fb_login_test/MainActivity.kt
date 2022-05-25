0de2981a9d0db306147c2bece23c33d7db5525b8
shinyelee <shinye0213@naver.com>
2022-05-25 20:47:28 +0900

    MainActivity error

--- app/src/main/java/com/shinyelee/fb_login_test/MainActivity.kt
+++ app/src/main/java/com/shinyelee/fb_login_test/MainActivity.kt
@@ -1,14 +1,8 @@
 package com.shinyelee.fb_login_test
 
-import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.DatePicker
-import android.widget.TextView
 import java.util.*
-import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
